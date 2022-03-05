bdfa70da5bb4e2480920ee41a76d0d663172c13c
Plertz <nickydeboer2005@gmail.com>
2022-03-05 22:41:33 +0100

    why do i do this

---
+++ includes/game/search.php
@@ -0,0 +1,26 @@
+<?php
+    function search_player($this_pnum){
+        $ohter_pnum = null;
+        
+        if($this_pnum % 2 == 0){
+            $other_pnum = $this_pnum - 1;
+        }
+        else{
+            $other_pnum = $this_pnum + 1;
+        }
+        $ohter_pnum = strval($ohter_pnum);
+        $file_path = "../../database/active_players/" . $other_pnum . ".json";
+        echo $file_path;
+        try{
+            $file = fopen($file_path, "wr");
+            $data = fgets($file);
+            fclose($file);
+            echo $data;
+        }
+        catch(Exception $e){
+            return null;
+        }
+        echo "hello";
+    }
+    search_player(2)
+?>
