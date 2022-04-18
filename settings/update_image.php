22caf2ab71b812b483c6e33594f76461dd2631a5
Plertz <nickydeboer2005@gmail.com>
2022-04-19 01:22:28 +0200

    img change gui update

--- settings/update_image.php
+++ settings/update_image.php
@@ -26,12 +26,51 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../includes/css/style.css">
+    <script src="../includes/js/navbar.js" defer></script>
     <title>Change icon | Chess online</title>
 </head>
 <body>
-    <form action="update_image.php" method="post" enctype="multipart/form-data" accept="image/jpg, image/png, image/gif">
+    <form class="change-img" action="update_image.php" method="post" enctype="multipart/form-data" accept="image/jpg, image/png, image/gif">
+        <h1> Change profile image</h1>
+        <h5>Images only</h5>    
         <input name="file" type="file">
         <input type="submit" value="submit">
     </form>
+
+    <div class="nav-background">
+    </div>
+    <div class="show-nav">
+        <span class="span-nav">
+            >
+        </span>
+    </div>
+    <nav class="nav">
+        <div class="account-header">
+            <p><a href="../settings/">Account</a></p>
+        </div>
+        
+        <ul>
+            <li>
+                <h1>General</h1>
+                <ul>
+                    <li><a href="../">Home</a></li>
+                </ul>
+            </li>
+            <li>
+                <h1>Game</h1>
+                <ul>
+                    <li><a href="../game/pvp">PvP Online</a></li>
+                    <li><a href="../game/offline">Pvp Offline</a></li>
+                    <li><a href="../game/bot/">Bot</a></li>
+                </ul>
+            </li>
+            <li>
+                <h1>Stats</h1>
+                <li><a href="../players/players.php">Players</a></li>
+                <li><a href="../leaderboard/">Leaderboard</a></li>
+            </li>
+        </ul>
+    </nav>
 </body>
 </html>
