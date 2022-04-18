75534831e2e05abc7cf81119fb7fbf95fa8b0b7d
Plertz <nickydeboer2005@gmail.com>
2022-04-19 01:07:54 +0200

    change password gui update

--- settings/update_password.php
+++ settings/update_password.php
@@ -11,10 +11,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../includes/css/style.css">
+    <script src="../includes/js/navbar.js" defer></script>
     <title>Change Password | Chess online</title>
 </head>
 <body>
-    <form action="../includes/settings/update_password.php" method="post">
+    <form class="change-password" action="../includes/settings/update_password.php" method="post">
         <label for="old_password">Old Password</label>
         <input type="password" name="old_password" id="old_password">
         <label for="new_password">New Password</label>
@@ -23,5 +25,40 @@
         <input type="password" name="confirm_password" id="confirm_password">
         <input type="submit" name="submit" value="Change Password">
     </form>
+
+    <div class="nav-background">
+    </div>
+    <div class="show-nav">
+        <span class="span-nav">
+            >
+        </span>
+    </div>
+    <nav class="nav">
+        <div class="account-header">
+            <p><a href="../settings/">Account</a></p>
+        </div>
+        
+        <ul>
+            <li>
+                <h1>General</h1>
+                <ul>
+                    <li><a href="../">Home</a></li>
+                </ul>
+            </li>
+            <li>
+                <h1>Game</h1>
+                <ul>
+                    <li><a href="../game/pvp">PvP Online</a></li>
+                    <li><a href="../game/offline">Pvp Offline</a></li>
+                    <li><a href="../game/bot/">Bot</a></li>
+                </ul>
+            </li>
+            <li>
+                <h1>Stats</h1>
+                <li><a href="../players/players.php">Players</a></li>
+                <li><a href="../leaderboard/">Leaderboard</a></li>
+            </li>
+        </ul>
+    </nav>
 </body>
 </html>
